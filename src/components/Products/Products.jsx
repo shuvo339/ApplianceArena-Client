55a779c4a4ee149a6254fb1bbc7ec15a629299d6
shuvo339 <shubhroshafiullah@gmail.com>
2024-08-15 20:12:48 +0600

    get products data and search & sort implemented

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import axios from "axios";
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+    const [search, setSearch] = useState('');
+    const [sort, setSort]=useState('');
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 10;
+
+    const url = `http://localhost:5000/products?search=${search}&page=${currentPage}&size=${itemsPerPage}&sort=${sort}`;
+    useEffect(() => {
+        axios(url)
+            .then(data => {
+                setProducts(data.data)
+                setLoading(false)
+            })
+    }, [search, currentPage, url, sort])
+
+    useEffect(() => {
+        const getCount = async () => {
+            const { data } = await axios(
+                `http://localhost:5000/productcount?search=${search}`
+            )
+
+            setCount(data.count)
+            setCurrentPage(1)
+        }
+        getCount()
+    }, [search])
+
+    const numberOfPages = Math.ceil(count / itemsPerPage)
+    const pages = [...Array(numberOfPages).keys()].map(p => p + 1);
+
+    const handleSearch = e => {
+        e.preventDefault();
+        const text = e.target.search.value;
+        setSearch(text);
+        setCurrentPage(0)
+    }
+
+    const handlePrev = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+        }
+    }
+    const handleNext = () => {
+        if (currentPage < pages.length) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+    const handleSort=e=>{
+        setSort(e.target.value);
+        console.log(e.target.value)
+    }
+
+    if (loading) {
+        return <span className="loading loading-ring w-[200px] flex justify-center items-center mt-16 mx-auto"></span>
+    }
+
+    return (
+        <div className="my-10">
+
+         <div className="flex flex-col md:flex-row  gap-4 md:gap-20  justify-around mb-6">
+               {/* sort */}
+               <select onChange={handleSort} className="w-full lg:w-72 select select-bordered" name="sort">
+                <option value="sort" disabled selected>Sort</option>
+                <option value="low">Low to High Price</option>
+                <option value="high">High to Low Price</option>
+                <option value="new">Newest First</option>
+            </select>
+
+            {/* search */}
+            <form onSubmit={handleSearch}>
+                <div className="w-full mb-6 mx-auto">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input type="text" name="search" className="grow" placeholder="Search" />
+                        <button className="btn btn-sm px-1 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-8 h-8 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg></button>
+                    </label>
+                </div>
+            </form>
+         </div>
+           
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {
+                products.map(product => <Product key={product._id} product={product}></Product>)
+            }
+            </div>
+
+            <div className="flex justify-center items-center mt-10">
+                <button onClick={handlePrev} className="btn mr-2 bg-slate-300 btn-sm">Prev</button>
+                {
+                    pages.map(page => <button onClick={() => setCurrentPage(page)} className={currentPage === page ? 'bg-[#9ACCC9] btn text-white mr-2 btn-sm' : 'btn bg-gray-800 mr-2 text-white btn-sm'} key={page}>{page}</button>)
+                }
+                <button onClick={handleNext} className="btn ml-2 bg-slate-300 btn-sm">Next</button>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
