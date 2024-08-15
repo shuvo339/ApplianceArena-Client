d25f133572d8d7514fc7e39c859881cd470c7252
shuvo339 <shubhroshafiullah@gmail.com>
2024-08-15 20:09:45 +0600

    footer added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar/Navbar";
+import Footer from "../pages/shared/Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
